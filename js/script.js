fb1b7f32e034ee5029f000c26bfe333f88ff24d7
Carlos Fernando Soto Zepeda <fernandosz.ti19@utsjr.edu.mx>
2022-05-17 11:07:26 -0500

    Subida del index, script y hoja de estilos

---
+++ js/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $("#btn1").on('click', function(e){
+        e.preventDefault();
+        alert("Esta pagina aún no esta disponible, seguimos trabajando :)")
+    });
+});
